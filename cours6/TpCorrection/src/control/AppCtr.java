08a20d5cc671656125f4708592cc9acf499e167a
Ana-ramahef <59668599+Ana-ramahef@users.noreply.github.com>
2021-09-22 17:14:39 -0400

    refining filewriting and console display

--- cours6/TpCorrection/src/control/AppCtr.java
+++ cours6/TpCorrection/src/control/AppCtr.java
@@ -23,92 +23,100 @@ public class AppCtr {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        final String FILEPATH ="json/jsontpa18test.json";
-    //lire le fichier Json 
-    String myJson;
-    try {
-            myJson = FileReader.loadFileIntoString(FILEPATH);
-            
-            //creation des objets json
-        JSONObject mainJSON = JSONObject.fromObject(myJson);
-        JSONArray ListeItemLotissement = mainJSON.getJSONArray("lotissements");
-        JSONObject lotissement ;
-            //recupere les donne du json
-        int typeTerrain = mainJSON.getInt("type_terrain");
-        double prixTerrainMin = mainJSON.getDouble("prix_m2_min");
-        double prixTerrainMax = mainJSON.getDouble("prix_m2_max");
-        String description=null ;
-        
-        double superficie =0 ;
         Date dateMesure = new Date();
         String dateMesureEnString;
         int nbServiceBase = 2;
-        double nbDroitPassage = 0 ; 
+        double nbDroitPassage = 0;
         double tauxTaxeMunicipale = 0.025;
         double tauxTaxeScolaire = 0.012;
         double montantFixe = 733.77;
         double montantPassageBase = 500.00;
         double valeurFonciere = 0;
         double montantService = 0;
-        double droitPassage=0;
+        double droitPassage = 0;
         double valeurLot = 0;
         double valeurTerrain = 0;
         double taxeMunicipale;
         double taxeScolaire;
+        double superficie = 0;
+        int nbServices = 0;
         //prixspuerficie = prixLot;
-        
-       
-            
-            for(int i = 0 ; i < ListeItemLotissement.size(); i ++){
-                
+        final String FILEPATH = "json/jsontpa18test.json";
+        //lire le fichier Json 
+        String myJson;
+        try {
+            myJson = FileReader.loadFileIntoString(FILEPATH);
+
+            //creation des objets json
+            JSONObject mainJSON = JSONObject.fromObject(myJson);
+            JSONArray ListeItemLotissement = mainJSON.getJSONArray("lotissements");
+            JSONObject lotissement;
+            //recupere les donne du json
+            int typeTerrain = mainJSON.getInt("type_terrain");
+            double prixTerrainMin = mainJSON.getDouble("prix_m2_min");
+            double prixTerrainMax = mainJSON.getDouble("prix_m2_max");
+
+            String description = null;
+
+            for (int i = 0; i < ListeItemLotissement.size(); i++) {
+                lotissement = ListeItemLotissement.getJSONObject(i);
+                description = lotissement.getString("description");
+                nbDroitPassage = lotissement.getDouble("nombre_droits_passage");
+                superficie = lotissement.getDouble("superficie");
+                nbServices = lotissement.getInt("nombre_services");
+                int nbServiceTotal = nbServices + nbServiceBase;
                 //CALCULER la valuer par lot
                 //valeur = (superficie + (prixMx+pirMin)/2) + droits de passage + montant pour les services
-                if(typeTerrain == 1 ){
-                    double prixSuperficie = (superficie * (( prixTerrainMax+prixTerrainMin)/2)) + 733.77 ;
+                if (typeTerrain == 1) {
+                    double prixSuperficie = (superficie * (prixTerrainMax + prixTerrainMin) / 2) + 733.77;
                     //rajouter formule pour arrondir a 0.5 pret;
-                    droitPassage = montantPassageBase - (nbDroitPassage *(0.05* valeurLot));
+                    droitPassage = 500 - (nbDroitPassage * (0.05 * valeurLot));
                     valeurFonciere = prixSuperficie + droitPassage;
-                    if(superficie<500){
+                    if (superficie < 500) {
                         montantService = 0;
-                    }else if((superficie> 500) ||(superficie<1000)){
-                        montantService = 500;
-                    }else{
-                        montantService = 1000;
+                    } else if ((superficie > 500) || (superficie < 1000)) {
+                        montantService = 500 * nbServiceTotal;
+                    } else {
+                        montantService = 1000 * nbServiceTotal;
                     }
                     valeurFonciere += montantService;
                     System.out.println(valeurFonciere);
-                }else if(typeTerrain == 2 ){
-                    double prixSuperficie = (superficie * prixTerrainMax ) +733.77;
+
+                } else if (typeTerrain == 2) {
+                    double prixSuperficie = (superficie * prixTerrainMax) + 733.77;
                     //rajouter formule pour arrondir a 0.5 pret;
-                    droitPassage = montantPassageBase - (nbDroitPassage *(0.1* valeurLot));
+                    droitPassage = 500 - (nbDroitPassage * (0.1 * valeurLot));
                     valeurFonciere = prixSuperficie + droitPassage;
-                   if(superficie<500){
-                       montantService = 500;
-                   }else{
-                       montantService = 1500;
-                   }
-                   valeurFonciere += montantService;
+                    if (superficie < 500) {
+                        montantService = 500;
+                    } else {
+                        if (nbServiceTotal < 4) {
+                            montantService = 1500 * nbServiceTotal;
+                        } else {
+                            montantService = 5000;
+                        }
+                    }
+                    valeurFonciere += montantService;
                     System.out.println(valeurFonciere);
-                }else if (typeTerrain ==0){
-                    double prixSuperficie = (superficie * prixTerrainMin ) +733.77;
+                } else if (typeTerrain == 0) {
+                    double prixSuperficie = (superficie * prixTerrainMin) + 733.77;
                     //rajouter formule pour arrondir a 0.5 pret;
-                    droitPassage = montantPassageBase - (nbDroitPassage *(0.15* valeurLot));
+                    droitPassage = 500 - (nbDroitPassage * (0.15 * valeurLot));
                     montantService = 0;
                     valeurFonciere = prixSuperficie + droitPassage;
                     valeurFonciere += montantService;
                     System.out.println(valeurFonciere);
-                }else{
+                } else {
                     System.out.println("ERROR");
                 }
-                
+
                 valeurTerrain += valeurFonciere;
             }
-        
+
             System.out.println("La valeur du terrain est :" + valeurTerrain);
-            System.out.println(mainJSON );
-            for(int i =0; i < ListeItemLotissement.size(); i ++)
-            {
-       
+            System.out.println(mainJSON);
+            for (int i = 0; i < ListeItemLotissement.size(); i++) {
+
                 lotissement = ListeItemLotissement.getJSONObject(i);
                 description = lotissement.getString("description");
                 nbDroitPassage = lotissement.getDouble("nombre_droits_passage");
@@ -116,46 +124,43 @@ public class AppCtr {
                 dateMesureEnString = lotissement.getString("date_mesure");
                 //traiter la date
                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                        try {
-                            dateMesure = dateFormat.parse(dateMesureEnString);
-                        } catch (ParseException ex) {
-                            Logger.getLogger(AppCtr.class.getName()).log(Level.SEVERE, null, ex);
-                        }
+                try {
+                    dateMesure = dateFormat.parse(dateMesureEnString);
+                } catch (ParseException ex) {
+                    Logger.getLogger(AppCtr.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 //valeur par lot    
-       System.out.println("lotissement ajouter " + lotissement.toString());
-            }  
-             JSONObject valeurFonciereTerrain = new JSONObject();
+                System.out.println("lotissement ajouter " + lotissement.toString());
+            }
+            ;
 
-            taxeScolaire = Utilities.calculerTaxeScolaire();
-            taxeMunicipale = Utilities.calculerTaxeMunicipale();
+            JSONObject valeurFonciereTerrain = new JSONObject();
 
-            valeurFonciereTerrain.accumulate("valeur_fonciere_totale", valeurTerrain +"$");
-            valeurFonciereTerrain.accumulate("taxe_scolaire", taxeScolaire+"$");
-            valeurFonciereTerrain.accumulate("taxe_municipale", taxeMunicipale+"$");
+            taxeScolaire = Utilities.calculerTaxeScolaire(valeurTerrain, tauxTaxeScolaire);
+            taxeMunicipale = Utilities.calculerTaxeMunicipale(valeurTerrain, tauxTaxeScolaire);
+
+            valeurFonciereTerrain.accumulate("valeur_fonciere_totale", valeurTerrain + "$");
+            valeurFonciereTerrain.accumulate("taxe_scolaire", taxeScolaire + "$");
+            valeurFonciereTerrain.accumulate("taxe_municipale", taxeMunicipale + "$");
 
             JSONArray lotissementArray = new JSONArray();
             JSONObject singleLot = new JSONObject();
 
+            for (int i = 0; i < ListeItemLotissement.size(); i++) {
+                lotissement = ListeItemLotissement.getJSONObject(i);
+                description = lotissement.getString("description");
+                singleLot.accumulate("description", description);
+                singleLot.accumulate("valeur_par_lot", valeurFonciere + "$");
+                lotissementArray.add(singleLot);
 
- for(int i =0; i < ListeItemLotissement.size(); i ++)
-            {
-       
-            singleLot.accumulate("description", description);
-            singleLot.accumulate("valeur_par_lot", valeurFonciere+"$");
-            lotissementArray.add(singleLot);
+                singleLot.clear();
 
-            
-              singleLot.clear();
-
-           
             }
- valeurFonciereTerrain.accumulate("lotissement", lotissementArray);
+            valeurFonciereTerrain.accumulate("lotissement", lotissementArray);
             FileWriter.saveStringIntoFile(valeurFonciereTerrain.toString(), "json/fichierSortieTPA18.json");
         } catch (IOException ex) {
             Logger.getLogger(AppCtr.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
+
     }
 }
-    
-
